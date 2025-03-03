f42f909daf6f076d7973198a6118c4cb24fa03d4
Siva Prasad A <164894350+SivaPA08@users.noreply.github.com>
2025-03-03 16:22:45 +0530

    Update init.lua

--- nvim/siva/sivaplugins/init.lua
+++ nvim/siva/sivaplugins/init.lua
@@ -1,4 +1,4 @@
---this is to  ensure Packer is installed
+-- Ensure Packer is installed
 local ensure_packer = function()
     local fn = vim.fn
     local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
@@ -13,40 +13,46 @@ local packer_bootstrap = ensure_packer()
 
 -- Plugin installation
 require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'          
-    use 'neovim/nvim-lspconfig'          
-    use 'hrsh7th/nvim-cmp'               
-    use 'hrsh7th/cmp-nvim-lsp'           
-    use 'nvim-treesitter/nvim-treesitter' 
-    use 'windwp/nvim-autopairs'          
-    use 'nvim-lualine/lualine.nvim'      
-    use 'EdenEast/nightfox.nvim'         
+    use 'wbthomason/packer.nvim'          -- Plugin manager
+    use 'neovim/nvim-lspconfig'          -- LSP configuration
+    use 'hrsh7th/nvim-cmp'               -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp'           -- LSP completion source for nvim-cmp
+    use 'nvim-treesitter/nvim-treesitter' -- Syntax highlighting
+    use 'windwp/nvim-autopairs'          -- Auto-pairing
+    use 'nvim-lualine/lualine.nvim'      -- Statusline
+    use 'EdenEast/nightfox.nvim'         -- Nightfox theme
 
     if packer_bootstrap then
         require('packer').sync()
     end
 end)
 
-vim.o.number = true                
-vim.o.relativenumber = false        
-vim.o.background = "dark"           
+-- General Settings
+vim.o.number = true                 -- Enable line numbers
+vim.o.relativenumber = true         -- Disable relative line numbers
+vim.o.background = "dark"           -- Use dark background for themes
+vim.o.swapfile=false                --Disables swap files and prevent swap-related messages 
 
+-- Set the Nightfox theme to Carbonfox
 require('nightfox').setup({
     options = {
-        styles = { 
+        styles = { -- Customize code styles
             comments = "italic",
             keywords = "bold",
         },
         inverse = {
-            match_paren = true, 
+            match_paren = true, -- Invert match parens
         },
     }
 })
-vim.cmd [[colorscheme carbonfox]] 
+vim.cmd [[colorscheme carbonfox]] -- Apply the "Carbonfox" theme for super-black background
 
 -- LSP Configuration for C++
 local lspconfig = require('lspconfig')
 lspconfig.clangd.setup {}
+lspconfig.ts_ls.setup{} --for js
+lspconfig.html.setup{}
+lspconfig.cssls.setup{} --for css
 
 -- Autocompletion setup
 local cmp = require('cmp')
@@ -61,15 +67,24 @@ cmp.setup({
     },
 })
 
--- Auto-pairing setup
-require('nvim-autopairs').setup{}
+
+
+require('lspconfig').pyright.setup {}
+local cmp = require('cmp')
+cmp.setup({
+    sources = {
+        { name = 'nvim_lsp' },
+    },
+})
+
 
 -- Treesitter setup
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { "cpp", "c" }, -- Install C++ parsers
+    ensure_installed = { "cpp", "c" ,"python","javascript"  }, -- Install C++ parsers
     highlight = {
         enable = false,                -- Enable syntax highlighting
     },
+    debug=true,
 }
 
 -- Statusline configuration
@@ -78,8 +93,31 @@ require('lualine').setup {
         theme = 'nightfox',
         section_separators = '',
         component_separators = '',
+	icons_enabled=false,
     },
+
+
 }
 
+vim.cmd [[
+  highlight MsgArea guifg=#ffcc00 guibg=#1e1e2e
+  highlight WildMenu guifg=#1e1e2e guibg=#ffcc00
+]]
 
-require('options').setup()
+--This is for changing curor color 
+vim.o.guicursor = "n:block-Cursor"
+vim.api.nvim_set_hl(0, 'Cursor', { fg = 'yellow', bg = 'yellow' })
+
+
+vim.g.netrw_bufsettings = "noma nomod nu rnu"
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype == "netrw" then
+      vim.wo.number = true
+      vim.wo.relativenumber = true
+    end
+  end,
+})
+
